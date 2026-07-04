e5a38a25a1d1d9e5ba8d42703767df64e1ff1fd2
Daehyun Lee <186126570+DHmango@users.noreply.github.com>
2026-07-04 13:21:37 -0500

    Most basic theme editor app sort of works

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,133 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import RulesCard from './RulesCard'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-  const rulesList = [
-    ['c','',"--os-accent-nonary","#a64dff"],
-    ["c","","--os-accent-octonary","#def1fb"],
-  ]
-  const [ruleValues, setRuleValues] = useState<unknown>({
-    [rulesList[0][2]]: {selector:rulesList[0][2], value:rulesList[0][3]}
-  })
+  //const rulesList = [
+  //['c','',"--os-accent-nonary","#a64dff"],
+  //  ["c","","--os-accent-octonary","#def1fb"],
+  //]
+  const [ruleValues, setRuleValues] = useState<Record<string, string[]>>({'--test': ['c','','#a64dff'], 'hello':['c','','#123456']})
   const whenInputChanged = (id: string, value:string) => {
-    setRuleValues(({prevValues}:{prevValues:object}) => ({
-      ...prevValues, [id]:value,
+    setRuleValues((prevValues) => ({
+      ...prevValues, [id]:prevValues[id].map((oldValue, index) => index===2 ? value : oldValue),//map sets every array value to the result of what you givt it. index 2 means it only changes the index 2 thing in the array 'key':['c','','THIS']
     }))
   }
   return (
     <>
-      <RulesCard reportBack={whenInputChanged} dataType='c' selector='' ruleKey='--test' ruleValue='#ff00ff'></RulesCard>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      {Object.entries(ruleValues).map(([ruleKey, ruleData]) => (
+        <RulesCard reportBack={whenInputChanged} ruleKey={ruleKey} ruleValue={ruleData[2]} dataType={ruleData[0]} selector={ruleData[1]}/>
+      ))}
     </>
   )
 }
--- webapp/src/RulesCard.tsx
+++ webapp/src/RulesCard.tsx
@@ -2,12 +2,14 @@
 export default function RulesCard({reportBack, dataType, selector, ruleKey, ruleValue}:{reportBack: (id: string, value: string) => void;dataType: string; selector: string; ruleKey: string; ruleValue: string}) {
     if (dataType === 'c'){
         return (<>
+        <div className="border border-indigo-600">
         <label>
             <u>{ruleKey}</u> hex code:<input value={ruleValue} name="coolInput" onChange={(e) => reportBack(ruleKey, e.target.value)}/> 
         </label>
         <p>{selector}</p>
         <p>{ruleKey}</p>
         <p>{ruleValue}</p>
+        </div>
         </>)
     }
 
