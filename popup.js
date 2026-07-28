a69fb2df0b2013089e02ead886c83e726878842b
Daehyun Lee <186126570+DHmango@users.noreply.github.com>
2026-07-27 23:16:00 -0500

    you can change the themes mostly and also it selects the active theme

--- popup.js
+++ popup.js
@@ -1,28 +1,29 @@
 const darkSelect = document.getElementById("darkSelect");
 const lightSelect = document.getElementById("lightSelect");
+const saveButton = document.getElementById("saveButton");
+
+const lightTheme = await browser.storage.local.get("lightTheme");
+const darkTheme = await browser.storage.local.get("darkTheme");
 const themesList = [];
-browser.storage.local.getKeys().then((keys) => {
-  for (const key of keys) {
-    if (key.startsWith("theme-")) {
-      key = await browser.storage.local.get(key)
-      themesList.push(
-        `${key.slice(-2)}${awaitJSON.parse(key).name}`,
-      );
-    }
-  }
-});
-for (const themeName of themesList) {
-  if (themeName.startsWith(browser.storage.local.get("lightTheme"))) {
-    lightSelect.add(new Option(themeName, themeName.slice(0, 2), true));
-  }
-  {
-    lightSelect.add(new Option(themeName, themeName.slice(0, 2)));
-  }
-  if (themeName.startsWith(browser.storage.local.get("darkTheme"))) {
-    darkSelect.add(new Option(themeName, themeName.slice(0, 2), true));
-  } else {
-    darkSelect.add(new Option(themeName, themeName.slice(0, 2)));
+const keys = await browser.storage.local.getKeys();
+for (const key of keys) {
+  if (key.startsWith("theme-")) {
+    const value = await browser.storage.local.get(key);
+    themesList.push(`${key.slice(-2)}-${await JSON.parse(value[key]).name}`);
   }
 }
-const option = new Option("test", "value");
-darkSelect.add(option);
+console.log(lightTheme.lightTheme.slice(-2));
+console.log(darkTheme.darkTheme.slice(-2));
+for (const themeName of themesList) {
+  darkSelect.add(new Option(themeName, `theme-${themeName.slice(0, 2)}`));
+  lightSelect.add(new Option(themeName, `theme-${themeName.slice(0, 2)}`));
+}
+darkSelect.value = darkTheme.darkTheme
+lightSelect.value = lightTheme.lightTheme
+
+saveButton.addEventListener("click", function () {
+  browser.storage.local.set({
+    lightTheme: lightSelect.value,
+    darkTheme: darkSelect.value,
+  });
+});
